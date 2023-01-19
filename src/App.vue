9d95a5950018fb8e05bedd05fadea52417e40960
Chukwuka <chuxjpu@gmail.com>
2023-01-19 12:59:45 +0100

    this is a Vue-beginning combined with Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h4 class="bg-primary text-white text-center p-2">
+      {{ name }}'s To Do List
+    </h4>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col fw-bold">Task</div>
+        <div class="col-2 fw-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2">{{t.done}}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      name: "Ugo",
+      tasks:[{action:"Buy Flowers", done: false},
+             {action:"Get Shoes", done: false},
+             {action:"Collect Tickets", done: true},
+             {action:"Call Ade", done: false}]
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
