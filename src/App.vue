9fd8bbb0fb69d4b37f7beec29a84feb09747825f
Chukwuka <chuxjpu@gmail.com>
2023-01-19 17:39:55 +0100

    filtering feature completed

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,19 @@
         <div class="col-2 fw-bold">Done</div>
       </div>
       <!-- 2nd feature adding a filtering effect to rearrange the list each time a task is checked completed-->
-      <div class="row" v-for="t in tasks" v-bind:key="t.action">
-        <div class="col">{{t.action}}</div>
-        <div class="col-2">
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+        <div class="col">{{ t.action }}</div>
+        <div class="col-2 text-center">
           <!-- 1st feature adding a check box for easy responding useability -->
-          <input type="checkbox" v-model="t.done"  class="form-check-input"/>{{t.done}}
+          <input type="checkbox" v-model="t.done" class="form-check-input" />{{ t.done }}
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input v-model="hideCompleted" type="checkbox" class="form-check-input" />
+          <label class="form-check-label fw-bold">
+            Hide completed tasks
+          </label>
         </div>
       </div>
     </div>
@@ -21,16 +29,24 @@
 </template>
 
 <script>
+
 export default {
   name: "App",
   data() {
     return {
       name: "Ugo",
-      tasks:[{action:"Buy Flowers", done: false},
-             {action:"Get Shoes", done: false},
-             {action:"Collect Tickets", done: true},
-             {action:"Call Ade", done: false}]
+      tasks: [{ action: "Buy Flowers", done: false },
+      { action: "Get Shoes", done: false },
+      { action: "Collect Tickets", done: true },
+      { action: "Call Ade", done: false }],
+      hideCompleted: true
     };
   },
+  computed:{
+    filteredTasks(){
+      return this.hideCompleted ?
+      this.tasks.filter(t => !t.done) : this.tasks
+    }
+  }
 };
 </script>
